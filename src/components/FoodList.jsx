69c08bb0801f23a1f618c9116ca151691b1c0821
alinardz <alinagriselrodriguez@gmail.com>
2018-06-07 15:01:45 -0500

    filtro

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -4,7 +4,6 @@ import {FoodCard} from './FoodCard';
 export const FoodList = (props)=>{
     return(
         <div>
-           <h2>Lista de Comidas</h2>
            <div>
                 {props.foods.map((food, index)=>{
                     return(
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const SearchBar = ({onChange})=>{
     return(
         <div>
-            <input type="text" name="search" onChange={onChange}/>
+            <input type="text" name="search" onChange={onChange} placeholder="search"/>
         </div>
     )
 }
